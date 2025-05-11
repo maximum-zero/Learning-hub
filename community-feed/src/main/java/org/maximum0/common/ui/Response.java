d3c0adf71e4c7531992f2619fb927fa0c0b7b192
maximum0 <maximum.zero95@gmail.com>
2025-05-11 23:56:59 +0900

    feat(community-feed): 공통 Response 및 공통 Error Handler 추가

---
+++ community-feed/src/main/java/org/maximum0/common/ui/Response.java
@@ -0,0 +1,15 @@
+package org.maximum0.common.ui;
+
+import org.maximum0.common.domain.exception.ErrorCode;
+
+public record Response<T>(Integer code, String message, T data) {
+
+    public static <T> Response<T> ok(T data) {
+        return new Response<>(0, "ok", data);
+    }
+
+    public static <T> Response<T> error (ErrorCode error) {
+        return new Response<>(error.getCode(), error.getMessage(), null);
+    }
+
+}
